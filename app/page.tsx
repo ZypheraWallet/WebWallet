e302893ed5581c72ef96004772945ffe2c36dce7
Kollusion <kollusion.dark@gmail.com>
2025-09-01 21:15:45 +0700

    page - Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 "use client"
+
 import React from 'react';
 import BalanceCard from '@/components/wallet/sections/balanceCard';
 import FastAction from '@/components/wallet/sections/fastAction';
 import AccountsCard from '@/components/wallet/sections/accountsCard';
 import HistoryCard from '@/components/wallet/sections/historyCard';
 import ProfileCard from '@/components/wallet/sections/profileCard';
+
 import { useRedirectIfNotLoggedIn } from '@/utils/redirect/redirectIfNotLoggedIn';
 
-const page = () => {
+const Page = () => {
     useRedirectIfNotLoggedIn();
     return (
         <main className='mx-auto container min-h-screen max-lg:px-2 flex flex-col gap-6 lg:justify-center items-start'>
@@ -24,4 +26,4 @@ const page = () => {
     );
 };
 
-export default page;
+export default Page;
--- components/wallet/sections/fastAction.tsx
+++ components/wallet/sections/fastAction.tsx
@@ -6,7 +6,7 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 import { Button } from '@/components/ui/button';
-import { HandHelping, Plus, QrCode, Send, Settings } from 'lucide-react';
+import { HandHelping, Plus, QrCode, Send } from 'lucide-react';
 
 const FastAction = () => {
     return (
@@ -16,19 +16,19 @@ const FastAction = () => {
             </CardHeader>
             <CardContent className='w-full flex justify-center gap-6 lg:justify-between lg:py-5'>
                 <Button size={'iconxl'} variant={'ghost'} className='flex-col gap-1.5 text-[9px] font-medium '>
-                    <Send className='size-6'/>
+                    <Send className='size-6' />
                     Отправить
                 </Button>
                 <Button size={'iconxl'} variant={'ghost'} className='flex-col gap-1.5 text-[9px] font-medium'>
-                    <Plus className='size-6'/>
+                    <Plus className='size-6' />
                     Пополнить
                 </Button>
                 <Button size={'iconxl'} variant={'ghost'} className='flex-col gap-1.5 text-[9px] font-medium max-xl:hidden'>
-                    <HandHelping className='size-6'/>
+                    <HandHelping className='size-6' />
                     Запросить
                 </Button>
                 <Button size={'iconxl'} variant={'ghost'} className='flex-col gap-1.5 text-[9px] font-medium'>
-                    <QrCode className='size-6'/>
+                    <QrCode className='size-6' />
                     Сканировать
                 </Button>
             </CardContent>
