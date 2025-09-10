5cbb41d8018edf81443c1e0660f82e1253de854a
Kollusion <kollusion.dark@gmail.com>
2025-09-10 16:00:01 +0700

    404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+    return (
+        <section className='h-screen w-full flex justify-center items-center'>
+            <div className="text-center space-y-2 font-mono">
+                <h1 className="text-9xl font-black">404</h1>
+                <h2 className="text-4xl font-medium">Not Found</h2>
+            </div>
+        </section>
+    )
+}
